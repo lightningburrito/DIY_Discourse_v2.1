550abde5c2c1e01e9ad20258d1fd5c431745fc00
Dakota Feist <dakotafeist@gmail.com>
2016-10-01 16:03:18 -0500

    Made angular route and view work. OPA ftw!

---
+++ javascript/homeController.js
@@ -0,0 +1,7 @@
+/**
+ * Created by D-Beatz on 10/1/16.
+ */
+
+app.controller("homeController", function ($scope) {
+
+});
--- javascript/main_script.js
+++ javascript/main_script.js
@@ -2,8 +2,20 @@
  * Created by D-Beatz on 10/1/16.
  */
 
-var app = angular.module("discourse",[] );
+var app = angular.module("discourse",["ngRoute"]);
 
-app.controller("homeController", function ($scope) {
-
+app.config(function($routeProvider) {
+	$routeProvider
+		.when("/", {
+			templateUrl : "views/home.html",
+			controller : "homeController"
+		})
+		.when("/search",{
+			templateUrl : "views/search.html",
+			controller : "searchController"
+		})
+		.when("/about", {
+			templateUrl : "views/about.html",
+			controller : "aboutController"
+		});
 });
