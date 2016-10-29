fa8d8aa09c3a5a2c19ef4f2e646742716d14b640
Dakota Feist <dakotafeist@gmail.com>
2016-10-29 15:02:11 -0500

    Made angular route and view work. OPA ftw!

--- javascript/homeController.js
+++ javascript/homeController.js
@@ -2,6 +2,8 @@
  * Created by D-Beatz on 10/1/16.
  */
 
+var app = angular.module("discourse");
+
 app.controller("homeController", function ($scope) {
 
 });
--- javascript/searchController.js
+++ javascript/searchController.js
@@ -2,6 +2,8 @@
  * Created by D-Beatz on 10/1/16.
  */
 
-app.controller("searchController", function ($scope) {
+var app = angular.module("discourse");
 
-});
+app.controller("searchController", ["dialog", function ($scope) {
+
+}]);
