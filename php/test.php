bcdbdeae680bed1c8c39c38c9df1f0084cc8e539
adam <adam.lawman@gmail.com>
2017-01-21 18:28:02 -0600

    trying to get that sweet sweet data

--- php/test.php
+++ php/test.php
@@ -1,21 +1,7 @@
 <?php
-
-//require 'database_connections.php';
-
-/*function main()
-{
-	$conn = connect();
-	if(!$conn) {
-		echo "connection failed";
-		return 0;
-	}
-	$stmt = $conn->prepare("SELECT * FROM cinfo WHERE id LIKE '1'");
-	$stmt->execute();
-	$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-	disconnect($conn);
-	echo json_encode($result);
-}
-main();*/
+require 'database_connections.php';
+echo "hello?";
+/*
 	$data = new stdClass();
 	$data->id = "42";
 	$data->author = "Butters";
@@ -27,59 +13,23 @@ main();*/
 	$array[] = $data;
 	echo json_encode($array);
 	return 0;
-/*
-    //$conn = new mysqli("localhost", "root", "password");
-    if (mysqli_connect_errno())
-    {
-        printf("Connect failed: %s\n", mysqli_connect_error());
-        exit();
-    }
-
-    //get comment with id = 1
-    $sql = "SELECT * FROM cinfo WHERE id LIKE '1'";
-    $result = $conn->query($sql);
-    
-    if ($result->num_rows > 0)
-    {
-      while ($row = $result->fetch_assoc())
-      {
-          echo "gilded: " . $row["gilded"]. "<br>";
-          echo "author flair text: " . $row["author_flair_text"]. "<br>";
-          echo "author flair css class: " . $row["author_flair_css_class"]. "<br>";
-          echo "retrieved on: " . $row["retrieved_on"]. "<br>";
-          echo "ups: " . $row["ups"]. "<br>";
-          echo "subreddit id: " . $row["subreddit_id"]. "<br>";
-          echo "edited: " . $row["edited"]. "<br>";
-          echo "controversiality: " . $row["controversiality"]. "<br>";
-          echo "parent id: " . $row["parent_id"]. "<br>";
-          echo "subreddit: " . $row["subreddit"]. "<br>";
-          echo "body: " . $row["body"]. "<br>";
-          echo "created utc: " . $row["created_utc"]. "<br>";
-          echo "downs: " . $row["downs"]. "<br>";
-          echo "score: " . $row["score"]. "<br>";
-          echo "author: " . $row["author"]. "<br>";
-          echo "archived: " . $row["archived"]. "<br>";
-          echo "distinguished: " . $row["distinguished"]. "<br>";
-          echo "id: " . $row["id"]. "<br>";
-          echo "score hidden: " . $row["score_hidden"]. "<br>";
-          echo "name: " . $row["name"]. "<br>";
-          echo "link id: " . $row["link_id"]. "<br>";
-          echo "tags: " . $row["tags"]. "<br>";
-          echo "word count: " . $row["wordcount"]. "<br>";
-          echo "lwordcount: " . $row["lwordcount"]. "<br>";
-          echo "sentcount: " . $row["sentcount"]. "<br>";
-          echo "lix: " . $row["lix"]. "<br>";
-
-
-         echo json_encode($row);
-      }
-    
-    }
-
-    else
-    {
-        echo "0 results";
-    }
-    
-    
-    $conn->close();*/
+*/
+$sql = "SHOW TABLES FROM $dbname";
+$result = mysql_query($sql);
+if ($result)
+{
+echo "DB error, could not list tables\n";
+echo "MySQL Error: " . mysql_error();
+exit;
+}
+//while ($row = msql_fetch_row($result))
+//{
+    $data = new stdClass();
+    $data->id = $row[0];
+    $array = [];
+	$array[] = $data;
+	echo json_encode($array);
+	return 0;
+//}
+msql_free_result($result);
+?>
