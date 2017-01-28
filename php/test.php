3cfc1f2e62dd7840b86b6eb4ce46d4db45cd72e2
Dakota Feist <dakota.feist@intrinsii.com>
2017-01-28 14:47:22 -0600

    added to ui

--- php/test.php
+++ php/test.php
@@ -13,17 +13,12 @@ function search()
 		return 0;
 	}
 
-	$data = new stdClass();
-	$data->id = "42";
-	$data->author = "Butters";
-	$data->ups = "561";
-	$data->downs = "11";
-	$data->score = "550";
-	$data->body = "This comment is really intellijent";
-	$array = [];
-	$array[] = $data;
-	echo json_encode($array);
-	return 0;
+	$stmt = $conn->prepare("SELECT * FROM cinfo WHERE id=:id LIMIT 1");
+	$stmt->bindParam(":id", $id);
+	$id = "cnas8zv";
+
+	$stmt->execute();
+	echo json_encode($stmt->fetchAll());
 }
 
 search();
