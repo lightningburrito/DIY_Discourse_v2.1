b17e83e00a68d20c1141c6d4274a1c8a57bfceee
austinolson5 <austinolson5@hotmail.com>
2017-02-18 14:17:59 -0600

    updated test.php with keyword echo

--- php/test.php
+++ php/test.php
@@ -26,13 +26,19 @@ function search()
 
     //echo json_encode($stmt->fetchAll());
 
-	$data = json_decode(file_get_contents('php://input'));
-    $author = $data->special_data->author;
-    $stmt = $conn->prepare('SELECT * FROM cinfo WHERE author = ? LIMIT 5');
-    $stmt->bindParam(1, $author, PDO::PARAM_STR, 12);
-    $stmt->execute();
+	//$data = json_decode(file_get_contents('php://input'));
+    //$author = $data->special_data->author;
+    //$stmt = $conn->prepare('SELECT * FROM cinfo WHERE author = ? LIMIT 5');
+    //$stmt->bindParam(1, $author, PDO::PARAM_STR, 12);
+    //$stmt->execute();
 
-    echo json_encode($stmt->fetchAll());
+    //echo json_encode($stmt->fetchAll());
+
+    $data = json_decode(file_get_contents('php://input'));
+    $keyword = $data->main_data->string_params[1];
+    echo $keyword;
+
+    //if (strlen($keyword) > 0)
 }
 
 search();
