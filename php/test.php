b44437aae1103afcd116c79634690f9bd2f32ecd
austinolson5 <austinolson5@hotmail.com>
2017-02-21 09:30:52 -0600

    updated test.php with field variables

--- php/test.php
+++ php/test.php
@@ -36,14 +36,42 @@ function search()
     //echo json_encode($stmt->fetchAll());
 
     $data = json_decode(file_get_contents('php://input'));
+
+    $edited = $data->main_data->edited;
+    $archived = $data->main_data->archived;
+    $distinguished = $data->main_data->distinguished;
+    $scoreHidden = $data->main_data->score_hidden;
+
+    $retrievedOn = $data->numerical_data->retrieved_on;
+    $createdUTC = $data->numerical_data->created_utc;
+    $upvotes = $data->numerical_data->up_votes;
+    $downvotes = $data->numerical_data->down_votes;
+    $score = $data->numerical_data->score;
+    $gilded = $data->numerical_data->gilded;
+    $controversiality = $data->numerical_data->controversiality;
+
+    $subreddit = $data->special_data->subreddit;
+    $author = $data->special_data->author;
+    $commentID = $data->special_data->comment_id;
+    $subredditID = $data->special_data->subreddit_id;
+    $parentID = $data->special_data->parent_id;
+    $linkID = $data->special_data->link_id;
+    $name = $data->special_data->name;
+    $authorFlairText = $data->special_data->author_flair_text;
+    $authorFlairClass = $data->special_data->author_flair_class;
+
     foreach ($data->main_data->string_params as $param)
 	{
         $not = $param->not;
         $keyword = $param->keyword;
         $type = $param->type;
 	}
-    //$keyword = $data->main_data->string_params{"keyword"};
-    //echo $data;
+    foreach ($data->main_data->num_params as $param)
+    {
+        $operator = $param->operator;
+        $number = $param->number;
+        $type = $param->type;
+    }
 
     if (strlen($keyword) > 0)
     {
@@ -59,6 +87,8 @@ function search()
     {
         echo "No keyword was received";
     }
+
+
 }
 
 search();
