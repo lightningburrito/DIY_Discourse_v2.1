392169f68a2437bae2679a647a4da92727e0f525
Dakota Feist <dakota.feist@intrinsii.com>
2017-04-01 19:46:18 -0500

    don't touch my code dang it!

--- php/get_keywords.php
+++ php/get_keywords.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: D-Beatz
- * Date: 4/1/2017
- * Time: 2:21 PM
- */
+
+require_once "database_connections.php";
+
+function get_keywords()
+{
+	//do search magic
+	$conn = connect();
+	//how the connection is really made
+	if (!$conn) {
+		echo "conn failed";
+		return 0;
+	}
+	$data = json_decode(file_get_contents('php://input'));
+}
+
+get_keywords();
