2d70508114d4a335232c75c6876f3b78bafd5531
austinolson5 <austinolson5@hotmail.com>
2017-04-08 16:33:22 -0500

    updated get_tags_comments.php

--- php/get_tags_comments.php
+++ php/get_tags_comments.php
@@ -16,6 +16,21 @@ function get_tags_comments()
 
 	//joins the cinfo_tags and cinfo tables and gets all the comments where the id_tag is the id of the
 	//tag->id
+
+    //find id of tag in tags table
+    $findTagStmt = $conn->prepare("SELECT * FROM tags WHERE name = :name LIMIT 1");
+    $findTagStmt->bindParam(':name', $tag);
+    $findTagStmt->execute();
+    $result = $findTagStmt->fetch();
+    $tagsTableId = $result["id"];
+
+    //join cinfo and cinfo_tags
+    $joinStmt = $conn->prepare("SELECT * FROM cinfo
+                            INNER JOIN cinfo_tags ON cinfo_tags.id_tags = :tagsTableId");
+    $joinStmt->bindParam(':tagsTableId', $tagsTableId);
+    $joinStmt->execute();
+
+    echo json_encode($joinStmt->fetchAll());
 }
 
 get_tags_comments();
